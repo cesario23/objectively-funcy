a3dcb410c4c96c5db2410d8d3b75d5aa58d36b88
cesar juarez <cesar.juarez@codeimmersives.com>
2021-03-11 12:21:10 -0800

    code

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 // Your code here.
-
-
-
-
+function getFirstName (object){
+  return object.firstName 
+}
+function getLastName (object){
+  return object.lastName
+}
+function getFullName (object){
+  return `${object.firstName} ${object.lastName}`;
+}
+function setFirstName (object, name){
+object.firstName = name;
+}
+function setAge (object, age){
+object.age = age
+}
+function giveBirthday (object){
+if (object.age){
+   const age = object.age + 1;
+  return object.age = age
+}else {
+  return object.age = 1
+}
+}
+function marry (person1, person2){
+person1.married = true;
+person2.married = true;
+person1.spouseName = person2.firstName + " " + person2.lastName;
+person2.spouseName = person1.firstName + " " + person1.lastName;
+}
+function divorce(person1, person2){
+person1.married = false;
+person2.married = false;
+delete person1.spouseName;
+delete person2.spouseName;
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
